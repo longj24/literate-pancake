a9ec98160bd79bb166fff4b735326ca7c0b6fe14
longj24 <justinlong312@yahoo.com>
2017-01-18 18:45:54 -0500

    Initial Commit

---
+++ Dogs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dogs
+//
+//  Created by Justin Long on 1/18/17.
+//  Copyright © 2017 Justin Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
